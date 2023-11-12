7c0c23ca803e9ec17fbfe1093962aadb7bb8ce85
overflowy <overflowy@riseup.net>
2023-11-12 22:26:18 +0100

    Add punctuation

--- config.py
+++ config.py
@@ -18,9 +18,7 @@ class DottyConfig:
             with open(CONFIG_PATH, "w", encoding="utf-8") as f:
                 yaml.safe_dump(self.data, f, indent=2, sort_keys=False)
                 if print_msg:
-                    rich.print(
-                        "✅ [green]Saved config file at[/green]", str(CONFIG_PATH)
-                    )
+                    rich.print("✅ [green]Saved config file at[/green]", CONFIG_PATH)
         except PermissionError:
             raise DottyConfigException(
                 "Cannot save config file. File might be in use by another process."
@@ -43,7 +41,7 @@ class DottyConfig:
 
     def create(self, dotfiles_dir: str):
         if CONFIG_PATH.exists():
-            rich.print("❌ [red]A config file already exists[/red]")
+            rich.print("❌ [red]A config file already exists[/red].")
             ok = Prompt.ask(
                 "[blue]Do you want to overwrite it[/blue]?", choices=["y", "n"]
             )
@@ -53,19 +51,22 @@ class DottyConfig:
         dotfiles_dir_path = Path(dotfiles_dir).resolve()
 
         if not dotfiles_dir_path.exists():
-            rich.print("❌ [red]Dotfiles directory does not exist[/red]")
+            rich.print("❌ [red]Dotfiles directory does not exist[/red].")
             ok = Prompt.ask(
                 "[blue]Do you want to create it[/blue]?", choices=["y", "n"]
             )
             if ok == "y":
                 dotfiles_dir_path.mkdir(parents=True)
 
-        self.data = {"dotfiles_dir": str(dotfiles_dir_path.as_posix()), "dotfiles": {}}
+        self.data = {"dotfiles_dir": dotfiles_dir_path.as_posix(), "dotfiles": {}}
         self.save()
 
     def add(self, dotfile: str):
         dotfile_path = Path(dotfile).resolve()
         short_uuid = str(uuid4())[:6]
-        self.data["dotfiles"][str(dotfile_path.as_posix())] = short_uuid
+        self.data["dotfiles"][dotfile_path.as_posix()] = short_uuid
         rich.print("✅ [green]Added dotfile[/green]:", dotfile_path.name)
         self.save(print_msg=False)
+
+    def remove(self, dotfile_or_uuid: str):
+        pass
