886160389a8484b12c07756620018d0d2d03ef6b
overflowy <overflowy@riseup.net>
2023-11-12 19:29:22 +0100

    Add DottyConfig class

--- config.py
+++ config.py
@@ -1,33 +1,48 @@
 import functools
 from pathlib import Path
-from typing import TypedDict
 
 import yaml
+
+from exceptions import DottyConfigException
 
 CONFIG_FILENAME = "dotty.yml"
 HOME_DIRECTORY = Path.home()
 CONFIG_PATH = HOME_DIRECTORY / CONFIG_FILENAME
 
 
-class DottyConfig(TypedDict):
-    dotfiles_dir: str
-    dotfiles: list[dict[str, str]]
+class DottyConfig:
+    def __init__(self):
+        self.load()
 
+    def save(self):
+        try:
+            with open(CONFIG_PATH, "w", encoding="utf-8") as f:
+                yaml.safe_dump(self.data, f)
+        except PermissionError:
+            raise DottyConfigException(
+                "Cannot save config file. File might be in use by another process."
+            )
 
-def load_config(CONFIG_PATH: Path) -> DottyConfig:
-    with open(CONFIG_PATH) as f:
-        return yaml.safe_load(f)
-
-
-def save_config(config):
-    with open(CONFIG_PATH, "w", encoding="utf-8") as f:
-        yaml.safe_dump(config, f)
+    def load(self):
+        try:
+            with open(CONFIG_PATH) as f:
+                self.data = yaml.safe_load(f)
+        except FileNotFoundError:
+            raise DottyConfigException(
+                "Config file not found. Run 'dotty init' to create one."
+            )
+        except yaml.YAMLError:
+            raise DottyConfigException(
+                "Config file is malformed. Run 'dotty init' to create a new one."
+            )
+        self.dotfiles_dir = Path(self.data["dotfiles_dir"])
+        self.dotfiles = self.data["dotfiles"]
 
 
 def with_config(func):
     @functools.wraps(func)  # type: ignore
     def wrapper(*args, **kwargs):
-        config = load_config(CONFIG_PATH)
+        config = DottyConfig()
         return func(config, *args, **kwargs)
 
     return wrapper
