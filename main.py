4291c6fb0ef43b9a3c3a91f3804a94f63a2d5679
overflowy <overflowy@riseup.net>
2023-11-12 14:22:46 +0100

    Add function to save configuration

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ HOME_DIRECTORY = Path.home()
 DEFAULT_CONFIG_PATH = HOME_DIRECTORY / CONFIG_FILENAME
 
 
-def load_config(config_path):
+def load_config(config_path: str) -> dict:
     with open(config_path) as f:
         return yaml.safe_load(f)
+
+
+def save_config(config, config_path):
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
