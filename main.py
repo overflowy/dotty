8dcfde4d9b9bff481dd1f76717b41856a0d354d8
overflowy <overflowy@riseup.net>
2023-11-13 23:54:43 +0100

    Refactor sync function

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from pathlib import Path
-from shutil import copy2
-
 import rich
 
 from config import DottyConfig
-from file_utils import (
-    FileCopyInstruction,
-    diff_paths,
-    to_backup_path,
-    validate_dotfile,
-)
+from file_utils import SyncUtil, validate_dotfile
 
 
 def init(dotfiles_dir: str, config=DottyConfig()):
@@ -23,25 +15,13 @@ def add(dotfile: str, config=DottyConfig()):
     config.add(dotfile)
 
 
-def sync(dry_run: bool = False, config=DottyConfig()):
-    rich.print("🔄 [blue]Syncing dotfiles[/blue]...")
-
-    copy_instructions: list[FileCopyInstruction] = []
-    config.load()
-
-    for dotfile in config.dotfiles:
-        dotfile_path = Path(dotfile)
-        backup_path = to_backup_path(dotfile_path, config.dotfiles_dir)
-        if paths := diff_paths(dotfile_path, backup_path):
-            copy_instructions.append(paths)
+def sync(dry_run: bool = False):
+    rich.print("💫 [blue]Syncing files[/blue]...")
+    sync_util = SyncUtil()
+    sync_util.prepare_sync()
 
     if dry_run:
+        rich.print("✅ [yellow]No changes made[/yellow].")
         return
 
-    if copy_instructions:
-        rich.print("📂 [blue]Copying files[/blue]...")
-    for instr in copy_instructions:
-        instr.dst.parent.mkdir(parents=True, exist_ok=True)
-        copy2(instr.src, instr.dst)
-
-    rich.print("✅ [green]All dotfiles are up to date[/green].")
+    sync_util.sync()
